018d42a5d516774c0f5b05e1a71f698808d29407
codingforf00d <jsnowman228@gmailcom>
2021-02-03 02:38:26 +0500

    copied code for list from internet

--- single-linked_list.c
+++ single-linked_list.c
@@ -1,1 +1,177 @@
 // Написать односвязный список [добавление в любое место, удаление, обращение по индексу]
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int value;
+    struct Node *next;
+} Node;
+
+Node *head = NULL;
+
+void push(Node **head, int data) {
+    Node *tmp = (Node*) malloc(sizeof(Node));
+    tmp->value = data;
+    tmp->next = (*head);
+    (*head) = tmp;
+}
+
+int pop(Node **head) {
+    Node* prev = NULL;
+    int val;
+    if (head == NULL) {
+        exit(-1);
+    }
+    prev = (*head);
+    val = prev->value;
+    (*head) = (*head)->next;
+    free(prev);
+    return val;
+}
+
+Node* getNth(Node* head, int n) {
+    int counter = 0;
+    while (counter < n && head) {
+        head = head->next;
+        counter++;
+    }
+    return head;
+}
+
+
+Node* getLast(Node *head) {
+    if (head == NULL) {
+        return NULL;
+    }
+    while (head->next) {
+        head = head->next;
+    }
+    return head;
+}
+
+
+void pushBack(Node *head, int value) {
+    Node *last = getLast(head);
+    Node *tmp = (Node*) malloc(sizeof(Node));
+    tmp->value = value;
+    tmp->next = NULL;
+    last->next = tmp;
+}
+
+Node* getLastButOne(Node* head) {
+    if (head == NULL) {
+        exit(-2);
+    }
+    if (head->next == NULL) {
+        return NULL;
+    }
+    while (head->next->next) {
+        head = head->next;
+    }
+    return head;
+}
+
+void popBack(Node **head) {
+    Node *lastbn = NULL;
+    //Получили NULL
+    if (!head) {
+        exit(-1);
+    }
+    //Список пуст
+    if (!(*head)) {
+        exit(-1);
+    }
+    lastbn = getLastButOne(*head);
+    //Если в списке один элемент
+    if (lastbn == NULL) {
+        free(*head);
+        *head = NULL;
+    } else {
+        free(lastbn->next);
+        lastbn->next = NULL;
+    }
+}
+
+int popBack(Node **head) {
+    Node *pFwd = NULL;  //текущий узел
+    Node *pBwd = NULL;  //предыдущий узел
+    //Получили NULL
+    if (!head) {
+        exit(-1);
+    }
+    //Список пуст
+    if (!(*head)) {
+        exit(-1);
+    }
+     
+    pFwd = *head; 
+    while (pFwd->next) { 
+        pBwd = pFwd;
+        pFwd = pFwd->next;
+    }
+ 
+    if (pBwd == NULL) {
+        free(*head);
+        *head = NULL;
+    } else {
+        free(pFwd->next);
+        pBwd->next = NULL;
+    }
+}
+
+
+void insert(Node *head, unsigned n, int val) {
+    unsigned i = 0;
+    Node *tmp = NULL;
+    //Находим нужный элемент. Если вышли за пределы списка, то выходим из цикла,
+    //ошибка выбрасываться не будет, произойдёт вставка в конец
+    while (i < n && head->next) {
+        head = head->next;
+        i++;
+    }
+    tmp = (Node*) malloc(sizeof(Node));
+    tmp->value = val;
+    //Если это не последний элемент, то next перекидываем на следующий узел
+    if (head->next) {
+        tmp->next = head->next;
+    //иначе на NULL
+    } else {
+        tmp->next = NULL;
+    }
+    head->next = tmp;
+}
+
+int deleteNth(Node **head, int n) {
+    if (n == 0) {
+        return pop(head);
+    } else {
+        Node *prev = getNth(*head, n-1);
+        Node *elm  = prev->next;
+        int val = elm->value;
+ 
+        prev->next = elm->next;
+        free(elm);
+        return val;
+    }
+}
+
+void deleteList(Node **head) {
+    Node* prev = NULL;
+    while ((*head)->next) {
+        prev = (*head);
+        (*head) = (*head)->next;
+        free(prev);
+    }
+    free(*head);
+}
+
+void fromArray(Node **head, int *arr, size_t size) {
+    size_t i = size - 1;
+    if (arr == NULL || size == 0) {
+        return;
+    }
+    do {
+        push(head, arr[i]);
+    } while(i--!=0);
+}
