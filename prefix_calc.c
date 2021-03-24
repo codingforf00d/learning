1e135f1134ab88f961d9d1d6a0aa1ea41af0bc19
codingforf00d <jsnowman228@gmailcom>
2021-03-24 07:31:23 +0500

    complete parsing

--- prefix_calc.c
+++ prefix_calc.c
@@ -41,25 +41,28 @@ int Calculate(int first_member, int second_member, char operator)
 void GetExpression(char *input)
 {
     Expr operations[100];
-    Expr object;
     int top_of_stack = 0;
     int inputLength = strlen(input);
     int tmp_index = 0;
     int previous_bracket_index, i, k;
-    char tmp_string[100];
+    char a, b;
     for (i = 0; i < inputLength; i++)
     {
         if (input[i] == '(' || input[i] == ')'){
+            char tmp_string[100];
+            Expr object;
             if (i > 0) {
                for (k = previous_bracket_index+1; k < i; k++){
                     push_tmp(input[k], tmp_string, tmp_index);
                     tmp_index++;
                } 
             }
+            sscanf(tmp_string, "%s %d", &object.operator, &object.value, inputLength);
+            push_operations(object, operations, top_of_stack);
+            top_of_stack++;
             previous_bracket_index = i;
         }
     }
-    printf("%s", tmp_string);
 }
 
 // достать верхний элемент стека
