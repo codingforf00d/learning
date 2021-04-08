9fccd67655fe5498fe6e5b7c4bb18e40a6915b8f
codingforf00d <jsnowman228@gmailcom>
2021-04-09 02:59:26 +0500

    parse operator, value wrong

--- prefix_calc.c
+++ prefix_calc.c
@@ -21,56 +21,22 @@ int main()
     GetExpression(input);
 }
 
-// Принимает оператор и члены алгебраического выражения
-int Calculate(int first_member, int second_member, char operator)
-{
-    switch (operator)
-    {
-    case '+':
-        return first_member + second_member;
-    case '-':
-        return first_member - second_member;
-    case '/':
-        return first_member / second_member;
-    case '*':
-        return first_member * second_member;
-    }
-}
-
 // Метод, преобразующий входную строку с выражением в стек с операторами и значениями
 void GetExpression(char *input)
-{
+{   
     Expr operations[100];
     int top_of_stack = 0;
     int inputLength = strlen(input);
-    int tmp_index = 0;
-    int previous_bracket_index, i, k;
-    char a, b;
-    for (i = 0; i < inputLength; i++)
-    {
-        if (input[i] == '(' || input[i] == ')'){
-            char tmp_string[100];
+    int i;
+    for (i=0; i<inputLength; i++){
+
+        if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/'){
             Expr object;
-            if (i > 0) {
-               for (k = previous_bracket_index+1; k < i; k++){
-                    push_tmp(input[k], tmp_string, tmp_index);
-                    tmp_index++;
-               } 
-            }
-            sscanf(tmp_string, "%s %d", &object.operator, &object.value, inputLength);
-            push_operations(object, operations, top_of_stack);
-            top_of_stack++;
-            previous_bracket_index = i;
+            sscanf(input+i, "%s %d", &object.operator, &object.value);
+            printf("operator: %s\nvalue: %d\n---------\n", &object.operator, &object.value);
         }
     }
-}
 
-// достать верхний элемент стека
-int pop(int index, char *stack)
-{
-    int data;
-    data = stack[index];
-    return data;
 }
 
 // добавить элемент в стек
@@ -83,6 +49,3 @@ void push_tmp(int data, char *stack, int index)
 {
     stack[index] = data;
 }
-
-// Редьюсер нашего стека алгебраических операций
-double Counting(char *input) {}
